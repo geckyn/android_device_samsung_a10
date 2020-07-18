9d1189c2d024d3a3b94751b20cf2e6cd29e316ca
geckyn <geckyn@gmail.com>
2020-07-18 16:16:28 +0000

    a10: BoardConfig: ARCH VARIANT is armv8-a
    Signed-off-by: geckyn <geckyn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ TARGET_NO_RADIOIMAGE := true
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
