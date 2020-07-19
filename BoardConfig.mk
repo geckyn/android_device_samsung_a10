0491ae2f7a337ad1d50cfc298285b455a82fc282
geckyn <geckyn@gmail.com>
2020-07-19 00:45:57 +0000

    a10: BoardConfig: force kernel toolchain path to aarch64 compiler since we running on 64bit kernel but 32bit system
    Signed-off-by: geckyn <geckyn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7885
 TARGET_KERNEL_CONFIG := a10_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 # Platform
