f6ac3271c361bd906d922b1bdd97f830d23894f3
geckyn <geckyn@gmail.com>
2020-07-27 16:05:57 +0000

    a10: update manifest
    Signed-off-by: geckyn <geckyn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,9 +107,7 @@ endif
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/android.hardware.cas@1.1-service.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # OTA Assert
 TARGET_OTA_ASSERT_DEVICE := a10
