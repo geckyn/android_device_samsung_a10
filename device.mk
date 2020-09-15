cd7fe7f5c604ebceee7522afe1e409e65a054d0c
geckyn <geckyn@gmail.com>
2020-09-15 12:32:48 +0000

    a10: build oss power hidl
    Signed-off-by: geckyn <geckyn@gmail.com>

--- device.mk
+++ device.mk
@@ -118,6 +118,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-impl.so \
+    android.hardware.power@1.0-service.exynos
+
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
