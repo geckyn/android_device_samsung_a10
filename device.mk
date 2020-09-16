938f3e02bd8c263e038c1a95126226c3e4a0dee9
geckyn <geckyn@gmail.com>
2020-09-16 15:40:31 +0000

    a10: build thermal 2.0 from samsung hardware
    Signed-off-by: geckyn <geckyn@gmail.com>

--- device.mk
+++ device.mk
@@ -151,8 +151,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    androdi.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@2.0-service.samsung
 
 # Treble
 PRODUCT_USE_VNDK_OVERRIDE := true
