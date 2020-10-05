6bc28bb2fc6e11d9987b2ba38f4afb96a918742a
geckyn <geckyn@gmail.com>
2020-10-05 11:40:28 +0000

    a10: charger: move files to configs
    Signed-off-by: geckyn <geckyn@gmail.com>

--- device.mk
+++ device.mk
@@ -81,10 +81,10 @@ PRODUCT_PACKAGES += \
 
 # Charger
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt \
-    $(LOCAL_PATH)/config/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
-    $(LOCAL_PATH)/config/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
-    $(LOCAL_PATH)/config/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png
+    $(LOCAL_PATH)/configs/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt \
+    $(LOCAL_PATH)/configs/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
+    $(LOCAL_PATH)/configs/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
+    $(LOCAL_PATH)/configs/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png
 
 # Graphics allocator
 PRODUCT_PACKAGES += \
